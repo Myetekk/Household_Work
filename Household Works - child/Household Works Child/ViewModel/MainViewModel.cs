bd60bacb7d342801e452662d307039c9eab1a6fa
szymonjakubiec <99356886+szymonjakubiec@users.noreply.github.com>
2023-07-03 21:18:45 +0200

    Stworzenie interfejsu dziecka

---
+++ Household Works - child/Household Works Child/ViewModel/MainViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Household_Works_Child.ViewModel
+{
+    //using Household_Works.ViewModel.BaseClass;
+    class MainViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler Property_Changed;
+
+        
+    }
+}
